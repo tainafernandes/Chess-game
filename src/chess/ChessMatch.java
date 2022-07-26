331018defa5dc809e453e577d6f2b223e8eb4896
tainafernandes <tainafefe@gmail.com>
2022-07-26 14:13:06 -0300

    Creation of the pieces and test on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +11,7 @@ public class ChessMatch {
 
     public ChessMatch(){ //Passo a informação que tem que ser um board de 8x8
         board = new Board(8, 8);
+        inicialSetup(); //Quando cria a partida, cria o tabuleiro e chama o inicialSetup
     }
 
     public ChessPiece[][] getPieces(){ //Retorna a matriz com as peças correspondentes a partida
@@ -18,5 +22,11 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void inicialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
     }
 }
