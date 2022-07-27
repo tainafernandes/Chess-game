b2876490311f20435bb119560b36a5f99e8b694f
tainafernandes <tainafefe@gmail.com>
2022-07-27 09:13:23 -0300

    Implementation of player switching every turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+    private int turn;
+    private Color currentPlayer;
     private Board board;
 
     public ChessMatch(){ //Passo a informação que tem que ser um board de 8x8
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         inicialSetup(); //Quando cria a partida, cria o tabuleiro e chama o inicialSetup
+    }
+
+    public int getTurn(){
+        return turn;
+    }
+
+    public Color getCurrentPlayer(){
+        return currentPlayer;
     }
 
     public ChessPiece[][] getPieces(){ //Retorna a matriz com as peças correspondentes a partida
@@ -36,6 +48,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece)capturedPiece;
     }
 
@@ -49,6 +62,9 @@ public class ChessMatch {
         if(!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece source position");
         }
+        if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("The chosen piece is not yours");
+        }
         if(!board.piece(position).isThereAnyPossibleMove()){ //NÃO tive movimento possível gero exceção
             throw new ChessException("There is no possible moves for the chosen piece");
         }
@@ -60,6 +76,11 @@ public class ChessMatch {
     }
     private void placeNewPiece(char column, int row, ChessPiece piece){ //operação de colocar peça passando a posição nas cordenadas do xadrez
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    private void nextTurn(){
+        turn ++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
     private void inicialSetup() {
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
